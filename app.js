a6d23a6ac20828b844a0a644890b5091f2214dc4
LEADR <ian.fennie@gmail.com>
2014-09-29 19:42:02 -0700

    Final commit

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-var myArray = [];
+$('document').ready(function() {
+  var myArray = [];
 
-for (var i = 0; i <= 99; i++) {
-  if ((i % 3) === 0) {
-    myArray[i] = "fizz";
-  } else if ((i % 5) === 0) {
-    myArray[i] = "buzz";
-  } else if (((i % 3) === 0) && ((i % 5) === 0)) {
-    myArray[i] = "fizz buzz";
-  } else {
-    myArray[i] = i;
+  for (var i = 0; i <= 99; i++) {
+    var num = i + 1;
+    if (((num % 3) + (num % 5)) === 0) {
+      myArray[i] = "fizz buzz";
+    } else if ((num % 3) === 0) {
+      myArray[i] = "fizz";
+    } else if ((num % 5) === 0) {
+      myArray[i] = "buzz";
+    } else {
+      myArray[i] = num;
+    }
+
+    $('<p>').html(myArray[i]).appendTo('.test');
   }
-}
-
-for (var i = 0; i <= 99; i++) {
-  var paragraph = $('<p>').html(myArray[i]);
-  $('body').append(paragraph);
-}
+});
